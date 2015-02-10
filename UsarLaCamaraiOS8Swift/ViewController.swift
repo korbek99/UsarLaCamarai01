53444e22c184e732aa0046ca90850367bc019b4e
Sergio Becerril <sesio@me.com>
2015-02-10 12:37:57 +0100

    Initial Commit

---
+++ UsarLaCamaraiOS8Swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UsarLaCamaraiOS8Swift
+//
+//  Created by Sergio Becerril on 10/2/15.
+//  Copyright (c) 2015 Sergio Becerril. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
